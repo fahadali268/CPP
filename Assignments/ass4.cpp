9bb67ac57859732d97ef2ad3a1c4f672520b707e
DevDnaysty <92596912+CyberNinjaaX@users.noreply.github.com>
2026-05-17 02:07:25 +0530

    ass.4

---
+++ Assignments/ass4.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        for(int j=2;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    // TrianglePattern
+    // bool val = true;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<val;
+    //         val = !val;
+    //     }
+    //     cout<<endl;
+    // }
+    // Rhombus patten 
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=n;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    return 0;
+}
