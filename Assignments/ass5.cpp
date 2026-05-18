b579069288556f192e23546363afabf3a8b48b9f
DevDnaysty <92596912+CyberNinjaaX@users.noreply.github.com>
2026-05-18 17:19:27 +0530

    Final ass.5

--- Assignments/ass5.cpp
+++ Assignments/ass5.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//sum of digits
 int digSum(int n){
     int res=0;
     while(n>0){
@@ -10,7 +11,58 @@ int digSum(int n){
     return res;
 }
 
+//parameters (a+b)^2
+int square(int a,int b){
+    int final = a*a+b*b+2*a*b;
+    return final;
+}
+
+//Lagest of 3 
+int largest(int a,int b,int c){
+    if(a>b && a>c){
+        return a;
+    }
+    else if(b>a && b>c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}
+
+// Next Character
+char nextChar(char ch){
+    if(ch == 'a'){
+        return 'z';
+    }
+    else{
+    ch += 1;
+    }
+    return ch;
+}
+
+//Palindrome
+int reverse(int n){
+    int res = 0;
+    while(n>0){
+        int lastdigit = n%10;
+        res = res*10 + lastdigit;
+        n = n/10;
+    }
+    return res;
+}
+
+bool isPalindrome(int n){
+    int revnum = reverse(n);
+    return n == revnum;
+}
+
 int main(){
-    cout<<"Sum of digits: "<<digSum(523)<<endl;
+    if(isPalindrome(122)){
+        cout<<"Palindrome"<<endl;
+    }
+    else{
+        cout<<"Not Palindrome"<<endl;
+    }
     return 0;
 }
