f849a7aa021ebfff3abcc87cee46e62fd5a2104b
fahadali268 <fahadalimohammed01@gmail.com>
2026-05-21 23:37:00 +0530

    dectobin

--- Functions/bintodec.cpp
+++ Functions/bintodec.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
 using namespace std;
 void bintodec(int n){
-    int bintodec = n;
+    int num1 = n;
     int dec = 0;
     int pow = 1;
-    while(n>0){
-        int lastdig = n%10;
+    while(num1>0){
+        int lastdig = num1%10;
         dec += lastdig*pow;
         pow = pow*2;
-        n = n/10;
+        num1 = num1/10;
     }
-    cout<<dec<<endl;
+    cout<<"Decimal no of ("<<n<<"): "<<dec<<endl;
 }
 
+void dectobin(int n){
+    int num = n;
+    int bin = 0;
+    int pow = 1;
+
+    while(num>0){
+        int rem = num%2;
+        bin += rem*pow;
+        pow = pow*10;
+        num = num/2;
+    }
+    cout<<"Binary no of ("<<n<<"): "<<bin<<endl;
+}
 int main(){
     bintodec(1011);
+    dectobin(7);
     return 0;
 }
