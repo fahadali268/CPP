ce9c0116ddf9dcf7662ab363a16696997e55e23c
fahadali268 <fahadalimohammed01@gmail.com>
2026-05-23 23:59:49 +0530

    *ptr++

--- demo.cpp
+++ demo.cpp
@@ -1,15 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 10829;
-    int sum = 0;
-    while(n>0){
-        int num = n % 10;
-        if(n%2!=0){
-        sum += num;
-        }
-        n = n / 10;
-    }
-    cout<<sum;
+    int a = 20;
+    int *ptr = &a;
+    ptr++;
+    cout<<ptr<<endl;
     return 0;
 }
